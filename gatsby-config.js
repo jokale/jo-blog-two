0bd4c4952e6301d92a0d1e9b4077010388d1e4f7
jokale <kalemajoanna@gmail.com>
2020-10-06 20:39:15 +0100

    change background colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
         name: `Gatsby Starter Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
-        background_color: `#ffffff`,
+        background_color: `#E8D4FE`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `content/assets/gatsby-icon.png`,
